16f81297a61bea6b5c4c1537b28750baa73a6584
Kosal Nandi <koshalnandi123@gmail.com>
2022-12-17 21:01:49 -0800

    created Constants class

---
+++ app/src/main/java/com/example/top250_movielist/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.top250_movielist
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://imdb-api.com/"
+        const val API_KEY = "k_hc2sam6f"
+    }
+}
