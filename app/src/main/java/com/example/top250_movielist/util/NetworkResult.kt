f24d523e839ddc8c534bba0bb3e32db8fa6b3de4
Kosal Nandi <koshalnandi123@gmail.com>
2022-12-18 17:42:07 -0800

    Create and inject MainViewModel class

--- app/src/main/java/com/example/top250_movielist/util/NetworkResult.kt
+++ app/src/main/java/com/example/top250_movielist/util/NetworkResult.kt
@@ -5,7 +5,7 @@ sealed class NetworkResult<T>(
     val message: String? = null
 ){
     class Success<T>(data: T): NetworkResult<T>(data)
-    class Error<T>(message: String?, data: T?): NetworkResult<T>(data,message)
+    class Error<T>(message: String?, data: T? = null): NetworkResult<T>(data,message)
     class Loading<T>: NetworkResult<T>()
 }
 
